58fff1da083a450abefcd45b1ccf93a5b061b532
emiroow <amir_esmaeelzadeh@yahoo.com>
2024-06-18 00:00:33 +0330

    push footer

--- src/components/templates/components/Footer.tsx
+++ src/components/templates/components/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+    <footer className="footer footer-center p-4 bg-base-100 text-base-content">
       <aside>
-        <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+        <p>Copyright © 2024 - Write by emiroow 🙂</p>
       </aside>
     </footer>
   );
